db8da69c87633c75cd12331207b0af9ef56af83e
p2r3 <p2r3@p2r3.com>
2025-12-10 11:02:45 +0000

    add more explicit canvas error handling

--- handlers/canvg.js
+++ handlers/canvg.js
@@ -67,6 +67,7 @@ async function doConvert (inputFile, inputFormat, outputFormat) {
 
   return await new Promise((resolve, reject) => {
     canvas.toBlob((blob) => {
+      if (!blob) reject("Canvas output failed");
       blob.arrayBuffer().then(buf => resolve(new Uint8Array(buf)));
     }, outputFormat.mime);
   });
